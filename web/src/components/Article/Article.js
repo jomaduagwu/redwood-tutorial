a8cbacb3fbda8d88b731a97acda65c9cd5c89f79
Ms Eko <jomaduagwu@gmail.com>
2024-01-31 13:02:34 -0600

    updated to display multiple comments

--- web/src/components/Article/Article.js
+++ web/src/components/Article/Article.js
@@ -1,5 +1,7 @@
 import { Link, routes } from '@redwoodjs/router'
 
+import CommentsCell from 'src/components/CommentsCell'
+
 const truncate = (text, length) => {
   return text.substring(0, length) + '...'
 }
@@ -13,7 +15,13 @@ const Article = ({ article, summary = false }) => {
         </h2>
       </header>
       <div className="mt-2 text-gray-900 font-light">
-        {summary ? truncate(article.body, 100) : article.body}</div>
+        {summary ? truncate(article.body, 100) : article.body}
+      </div>
+      {!summary && (
+        <div className="mt-12">
+          <CommentsCell />
+        </div>
+      )}
     </article>
   )
 }
